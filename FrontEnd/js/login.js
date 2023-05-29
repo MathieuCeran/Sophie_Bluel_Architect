4b3bad070bc7eec5faf2d2e1a45b15e9aa140801
Mathieu <97529184+MathieuCeran@users.noreply.github.com>
2023-05-29 11:06:00 +0200

    connexion page

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -15,7 +15,7 @@ async function connexionUser() {
   // traite la réponse de l'API pour savoir si connexion réussie ou non
   if (response.status === 200) {
     // stock réponse serveur qui représente les informations de l'utilisateur dans variable userInfos
-    console.log("ok");
+    console.log("ok"); // TODO :reste a traiter les informations
   } else {
     // affiche à l'user un message d'erreur quand mdp et/ou email incorrect
     alert("Le mot de passe et/ou l'e-mail est incorrecte");
